b8975233fa55abb78795eda8839e56283e8b8a75
nasrimohtadi@gmail.com <Awkward@009>
2020-04-09 21:13:54 +0200

    Added Book Entity and Language Enumeration

---
+++ src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -0,0 +1,107 @@
+package com.pluralsight.bookstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Book {
+
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private Float unitCost;
+    private String isbn;
+    private Date publicationDate;
+    private Integer nbOfPages;
+    private String imageURL;
+    private Language language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNbOfPages() {
+        return nbOfPages;
+    }
+
+    public void setNbOfPages(Integer nbOfPages) {
+        this.nbOfPages = nbOfPages;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", isbn='" + isbn + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", nbOfPages=" + nbOfPages +
+                ", imageURL='" + imageURL + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
---
+++ src/main/java/com/pluralsight/bookstore/model/Language.java
@@ -0,0 +1,6 @@
+package com.pluralsight.bookstore.model;
+
+public enum Language {
+
+    ENGLISH, FRENCH, SPANISH, ITALIAN, FINISH, GERMAN, RUSSIAN, DEUTSCH
+}
