2d3eaa1cc7ea1c07f08c227db5f832afb49b4e8d
nasrimohtadi@gmail.com <Awkward@009>
2020-04-11 00:51:50 +0200

    Test-persistence

--- src/test/java/com/pluralsight/bookstore/repository/BookRepositoryTest.java
+++ src/test/java/com/pluralsight/bookstore/repository/BookRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.pluralsight.bookstore.repository;
 
+import com.pluralsight.bookstore.model.Book;
+import com.pluralsight.bookstore.model.Language;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -24,11 +26,15 @@ public class BookRepositoryTest {
         assertEquals(Long.valueOf(0),bookRepository.countAll());
     }
 
-
+    /* to avoid error WELD-001474: ..BookRepository is on the classpath, but was ignored because a class it references was not found: com.pluralsight.bookstore.model.Book
+     * you have to add Book Class to ShrinWrap
+     */
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
                 .addClass(BookRepository.class)
+                .addClass(Book.class)
+                .addClass(Language.class)
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
